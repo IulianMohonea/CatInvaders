c167a549f2a8fde5cde7683823b35aa0c9aa9e7f
IulianMohonea <126501200+IulianMohonea@users.noreply.github.com>
2023-05-23 23:24:18 +0300

    Add files via upload

--- Errors.h
+++ Errors.h
@@ -6,14 +6,14 @@ class Errors :public std::runtime_error {
 
 class playerErrors :public Errors{
 private:
-    const std::string replica = "Miscare invalida: ";
+
 public:
     explicit playerErrors(std::string& mesaj);
 };
 
 class fireErrors : public Errors{
 private:
-    const std::string replica2 = "Timp prea putin intre gloante: ";
+
 public:
     explicit fireErrors(std::string& mesaj);
 };
